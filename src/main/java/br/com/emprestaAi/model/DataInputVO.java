9302ecee6b59ade2eb935d4ec9705ddfec7727c7
jardriel <jardriel.redes18@gmail.com>
2021-03-03 14:39:17 -0300

    fixes models

--- src/main/java/br/com/emprestaAi/model/DataInputVO.java
+++ src/main/java/br/com/emprestaAi/model/DataInputVO.java
@@ -1,17 +1,15 @@
 package br.com.emprestaAi.model;
 
-import lombok.NoArgsConstructor;
-
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.br.CPF;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
